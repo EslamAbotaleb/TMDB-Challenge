d2aeb49e0faa726f6137b8bab741031ab24e0ef2
Eslam  Baioumy <e.baiomy@mobily.com.sa>
2024-07-06 15:33:58 +0300

    prepare class for check connection network

---
+++ TMDB/Utilities/NetworkChecker.swift
@@ -0,0 +1,22 @@
+//
+//  NetworkChecker.swift
+//  TMDB
+//
+//  Created by Eslam on 06/07/2024.
+//
+
+import Foundation
+import Network
+
+@Observable
+final class NetworkMonitor {
+    private let networkMonitor = NWPathMonitor()
+    private let workerQueue = DispatchQueue(label: "Monitor")
+    var isConnected = false
+    init() {
+        networkMonitor.pathUpdateHandler = { path in
+            self.isConnected = path.status == .satisfied
+        }
+        networkMonitor.start(queue: workerQueue)
+    }
+}

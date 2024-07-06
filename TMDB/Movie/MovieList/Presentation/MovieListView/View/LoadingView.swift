697b41018985f3f0ac3f6ee17a9ffcb2d79c85ea
Eslam  Baioumy <e.baiomy@mobily.com.sa>
2024-07-06 21:34:18 +0300

    added LoadingView

---
+++ TMDB/Movie/MovieList/Presentation/MovieListView/View/LoadingView.swift
@@ -0,0 +1,26 @@
+//
+//  LoadingView.swift
+//  TMDB
+//
+//  Created by Eslam on 06/07/2024.
+//
+
+import SwiftUI
+struct LoadingView<Content>: View where Content: View {
+    @Binding var isShowing: Bool
+    @Binding var errorMessage: String
+
+    var content: ()-> Content
+
+    var body: some View {
+        GeometryReader {
+            geometry in
+            ZStack(alignment: .center) {
+                self.content()
+                    .disabled(self.isShowing)
+                    .blur(radius: self.isShowing ? 3 : 0)
+                Text(errorMessage)
+            }
+        }
+    }
+}

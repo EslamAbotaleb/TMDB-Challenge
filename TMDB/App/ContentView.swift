a26592e6c6c3001a0c9e9b436140a1c36bc80e21
Eslam  Baioumy <e.baiomy@mobily.com.sa>
2024-07-06 15:51:40 +0300

    Prepared the tabview for (now playing, popular, upcoming movies)

--- TMDB/App/ContentView.swift
+++ TMDB/App/ContentView.swift
@@ -9,14 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     @Environment(NetworkMonitor.self) private var networkMonitor
+    @State private var selectedTabIndex = 1
 
     var body: some View {
         VStack {
             if networkMonitor.isConnected {
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundStyle(.tint)
-                Text("Hello, world!")
+                NavigationStack {
+                    TabView(selection: $selectedTabIndex,
+                            content:  {
+                        Text("Now Playing").tabItem { Text("Now Playing") }.tag(1)
+                        Text("Popular").tabItem { Text("Popular") }.tag(2)
+                        Text("Upcoming Movies").tabItem { Text("Upcoming Movies") }.tag(3)
+                    })
+                    .onChange(of: selectedTabIndex) {
+
+                    }
+                }
             } else {
                 DisconnectConnectionView()
             }

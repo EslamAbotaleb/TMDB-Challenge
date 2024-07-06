1d9fc0237ac6a87f18e9e5b9f7dc593dbf06a3fc
Eslam  Baioumy <e.baiomy@mobily.com.sa>
2024-07-06 13:47:04 +0300

    Initial Commit

---
+++ TMDB/TMDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMDBApp.swift
+//  TMDB
+//
+//  Created by Eslam on 06/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct TMDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

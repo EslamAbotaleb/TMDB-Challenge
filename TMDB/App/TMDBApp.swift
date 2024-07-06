9a591a16c31013fef3ef40ea6a252c99992db81f
Eslam  Baioumy <e.baiomy@mobily.com.sa>
2024-07-06 15:37:12 +0300

    preapre state for check connectivity network & added environment network monitor to content view

--- TMDB/App/TMDBApp.swift
+++ TMDB/App/TMDBApp.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 @main
 struct TMDBApp: App {
+    @State private var networkMonitor = NetworkMonitor()
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(networkMonitor)
         }
     }
 }
